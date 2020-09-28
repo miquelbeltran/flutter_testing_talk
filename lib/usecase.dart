3ce614e2bdfcb940a967dde80b5bb29fb1907829
Miguel Beltran <m@beltran.work>
2020-09-28 07:00:59 +0200

    talk code

---
+++ lib/usecase.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter_testing_talk/api_service.dart';
+
+import 'model.dart';
+
+class UseCase {
+  final ApiService service;
+
+  UseCase(this.service);
+
+  Gif run() {
+    // todo
+  }
+}
